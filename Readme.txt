e58b404b0bbdea251f7a178c6d5ceaae704440de
Vanithasri1984 <vanithamani.pb@gmail.com>
2024-08-17 12:19:47 +0530

    start

--- Readme.txt
+++ Readme.txt
@@ -1,6 +1,6 @@
 Thanks for downloading this template!
 
-Template Name: Impact
-Template URL: https://bootstrapmade.com/impact-bootstrap-business-website-template/
+Template Name: Gardening
+Template URL: https://bootstrapmade.com/Gardening-bootstrap-business-website-template/
 Author: BootstrapMade.com
 License: https://bootstrapmade.com/license/
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 /**
-* Template Name: Impact
-* Template URL: https://bootstrapmade.com/impact-bootstrap-business-website-template/
+* Template Name: Gardening
+* Template URL: https://bootstrapmade.com/Gardening-bootstrap-business-website-template/
 * Updated: Aug 07 2024 with Bootstrap v5.3.3
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
